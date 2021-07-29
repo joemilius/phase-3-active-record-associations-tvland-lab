7de89c4a0222c5f3e060d5520ea67c7c0dc7fe8a
JoeMilius <joemilius@gmail.com>
2021-07-29 10:11:35 -0600

    Tests Passed

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def list_roles
+    character_name = characters.pluck(:name).first
+    show_name = shows.pluck(:name).first
+
+    "#{character_name} - #{show_name}"
+  end
 end
+
--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,5 +1,10 @@
 class CreateCharacters < ActiveRecord::Migration[6.1]
   def change
     # Your code here
+    create_table :characters do |t|
+      t.string :name
+      t.references :actor, foreign_key: true
+      t.references :show, foreign_key: true
+    end
   end
 end
